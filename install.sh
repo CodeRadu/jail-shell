d7dff2b6d598a62a1caf93ab0f274979a53af6f3
Radu <radu.gabi.m@gmail.com>
2022-08-12 14:52:20 +0000

    added architectures

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ if [ $1 = "uninstall" ]; then
   exit
 fi
 
-wget https://github.com/CodeRadu/jail-shell/releases/download/0.0.1/jailsh_0.0.1_amd64.deb -O jailsh.deb
+arch=$(dpkg --print-architecture)
+
+wget https://github.com/CodeRadu/jail-shell/releases/download/0.0.1/jailsh_0.0.1_$arch.deb -O jailsh.deb
 sudo apt install -y ./jailsh.deb
 rm jailsh.deb
 echo Done
